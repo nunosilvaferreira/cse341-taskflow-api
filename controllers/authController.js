a5d3b4c7dadb752963d0f376dd40bfede138b6e2
nunosilvaferreira <nferreira@byupathway.edu>
2025-10-03 16:58:17 +0100

    W05.1.5

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: (process.env.JWT_EXPIRES_IN || "30d").trim()
   });
 };
 
